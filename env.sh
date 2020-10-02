f0fdbe3acd163bb6eb0eafce0ab522c6153d4266
M1chlCZ <66187879+M1chlCZ@users.noreply.github.com>
2020-10-02 17:51:54 +0200

    Delete env.sh

--- env.sh
+++
@@ -1,9 +0,0 @@
-echo "Setting up enviromental commands..."
-mkdir .commands
-echo "export PATH="$PATH:/root/.commands"" >> ~/.profile
-cat > /root/.commands/hello << EOL
-#!/bin/bash    
-echo My first program
-EOL
-chmod +x /root/.commands/hello
-. ~/.profile
