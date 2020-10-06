0b82bf21c2c645cef7d933edbd1f9c86650c54ed
Michal Žídek <66187879+M1chlCZ@users.noreply.github.com>
2020-10-06 13:29:05 +0200

    Update command

--- ccashMNinstall.sh
+++ ccashMNinstall.sh
@@ -379,13 +379,43 @@ EOL
 
 cat > ~/.commands/campusUpdate << EOL
 #!/bin/bash    
-~/Campusd
+# Check if we are root
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root! Aborting..." 1>&2
+   exit 1
+fi
+
+killall Campusd
+rm Campusd
+
+export BDB_INCLUDE_PATH="/usr/local/BerkeleyDB.6.2/include"
+export BDB_LIB_PATH="/usr/local/BerkeleyDB.6.2/lib"
+
+cd ~
+git clone https://github.com/SaltineChips/CampusCash CampusCash
+cd ~/CampusCash/src
+chmod a+x obj
+chmod a+x leveldb/build_detect_platform
+chmod a+x secp256k1
+chmod a+x leveldb
+chmod a+x ~/CampusCash/src
+chmod a+x ~/CampusCash
+make -f makefile.unix USE_UPNP=-
+cd ~ 
+cp  ~/CampusCash/src/CampusCashd /root/Campusd
+cp  CampusCash/src/CampusCashd /root/Campusd #possible retarded fix
+sleep 5
+rm -r CampusCash
+
+read -p "Everything was updated" -n1 -s
+
 EOL
 
 chmod +x /root/.commands/getinfo
 chmod +x /root/.commands/mnstart
 chmod +x /root/.commands/mnstatus
 chmod +x /root/.commands/startd
+chmod +x /root/.commands/campusUpdate
 
 sleep 1
 
