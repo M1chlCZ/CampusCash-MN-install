0236e70cba3fda9dc2586349fe6c8844924a23e7
Michal Žídek <m1chlcz18@gmail.com>
2020-10-11 17:11:44 +0200

    v1.1

--- env.sh
+++ env.sh
@@ -5,10 +5,26 @@ rm /root/.commands/getinfo > /dev/null 2>&1
 rm /root/.commands/mnstart > /dev/null 2>&1
 rm /root/.commands/mnstatus > /dev/null 2>&1
 rm /root/.commands/startd > /dev/null 2>&1
+rm /root/.commands/stopd > /dev/null 2>&1
+rm /root/.commands/commandUpdate > /dev/null 2>&1
 rm /root/.commands/campusUpdate > /dev/null 2>&1
+rm /root/.commands/clearbanned > /dev/null 2>&1
 
 cat > ~/.commands/gethelp << EOL
 #!/bin/bash
+
+cat << "EOF"
+          |Brought to you by|         
+  __  __ _  ____ _   _ _     ____ _____
+ |  \/  / |/ ___| | | | |   / ___|__  /
+ | |\/| | | |   | |_| | |  | |     / / 
+ | |  | | | |___|  _  | |__| |___ / /_ 
+ |_|  |_|_|\____|_| |_|_____\____/____|
+       For complains Tweet @M1chl 
+
+CCASH: Ccbbd6uUZF2GD5wE5LEfjGPA3YWPjoLC6P
+
+EOF
 echo ""
 echo "Here is list of commands for you CampusCash service"
 echo "you can type these commands anywhere in terminal."
@@ -23,9 +39,15 @@ echo "mnstatus             | Status of the masternode"
 echo ""
 echo "startd               | Start CampusCash deamon"
 echo ""
+echo "stopd                | Stop CampusCash deamon"
+echo ""
 echo "campusUpdate         | Update CampusCash deamon"
 echo ""
+echo "commandUpdate        | Update List of commands"
+echo ""
 echo "getpeerinfo          | Show peer info"
+echo ""
+echo "clearbanned          | Clear all banned IPs"
 echo ""
 echo "gethelp              | Show help"
 echo "---------------------------------------------------"
@@ -55,7 +77,45 @@ EOL
 
 cat > ~/.commands/startd << EOL
 #!/bin/bash    
-~/Campusd
+systemctl start ccash.service > /dev/null 2>&1
+EOL
+
+cat > ~/.commands/stopd << EOL
+#!/bin/bash    
+systemctl stop ccash.service
+sleep 1
+if pgrep Campusd &> /dev/null ; then killall Campusd > /dev/null 2>&1 ; fi
+EOL
+
+cat > ~/.commands/clearbanned << EOL
+#!/bin/bash    
+~/Campusd clearbanned
+EOL
+
+cat > ~/.commands/commandUpdate << EOL
+#!/bin/bash    
+wget https://raw.githubusercontent.com/M1chlCZ/CampusCash-MN-install/main/env.sh > /dev/null 2>&1
+source env.sh
+clear
+
+cat << "EOF"
+            Update complete!
+
+           |Brought to you by|         
+  __  __ _  ____ _   _ _     ____ _____
+ |  \/  / |/ ___| | | | |   / ___|__  /
+ | |\/| | | |   | |_| | |  | |     / / 
+ | |  | | | |___|  _  | |__| |___ / /_ 
+ |_|  |_|_|\____|_| |_|_____\____/____|
+       For complains Tweet @M1chl 
+
+CCASH: Ccbbd6uUZF2GD5wE5LEfjGPA3YWPjoLC6P
+
+EOF
+
+. ~/.commands/gethelp
+
+echo ""
 EOL
 
 cat > ~/.commands/campusUpdate << EOL
@@ -120,13 +180,17 @@ read -p "You may need run mnstart command to start a masternode after update. Pr
 echo ""
 EOL
 
+
 chmod +x /root/.commands/getinfo
 chmod +x /root/.commands/mnstart
 chmod +x /root/.commands/mnstatus
 chmod +x /root/.commands/startd
+chmod +x /root/.commands/stopd
+chmod +x /root/.commands/commandUpdate
 chmod +x /root/.commands/campusUpdate
 chmod +x /root/.commands/gethelp
 chmod +x /root/.commands/getpeerinfo
+chmod +x /root/.commands/clearbanned
 
 . .commands/gethelp
 
