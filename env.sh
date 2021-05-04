b81b801d65eb9f96c3b92ee77bdd707f5b596540
Michal Žídek <m1chlcz18@gmail.com>
2021-05-04 20:16:59 +0200

    mnxstart fix

--- env.sh
+++ env.sh
@@ -373,8 +373,8 @@ EOL
 
 cat > ~/.commands/mnxstart << EOL
 #!/bin/bash    
-PORT=$1-1
-./Campusd -conf=/root/.CCASH$1/CampusCash.conf -datadir=/root/.CCASH$1 -port=1200$PORT masternode start
+PORT=\$1-1
+./Campusd -conf=/root/.CCASH\$1/CampusCash.conf -datadir=/root/.CCASH\$1 -port=1200\$PORT masternode start
 EOL
 
 cat > ~/.commands/mn2status << EOL
