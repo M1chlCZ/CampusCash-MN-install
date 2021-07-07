79176a127c9b774924493059c92d90542dc66fd7
Michal Žídek <m1chlcz18@gmail.com>
2021-07-07 13:34:54 +0200

    Updated repository

--- ccashMNinstall.sh
+++ ccashMNinstall.sh
@@ -265,7 +265,7 @@ mkdir /root/.CCASH
 if [[ ("$BOOTSTRAP" == "y" || "$BOOTSTRAP" == "Y" || "$BOOTSTRAP" == "") ]]; then
     echo "Downloading bootstrap..."
     cd ~/.CCASH
-    wget https://github.com/CampusCash/CampusCash_Release/releases/download/1.0.14.1/CCASH_bootstrap.zip
+    wget https://github.com/Pepsinside/CampusCash_Release/releases/download/1.1.0.2/CCASH_bootstrap.zip
     unzip CCASH_bootstrap.zip
     rm CCASH_bootstrap.zip
     cd ~
